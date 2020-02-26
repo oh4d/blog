9fea0cb5fcfd810ea937b0c9e1ff94cf79acb682
Efrat19 <efrat890089@gmail.com>
2020-02-26 02:43:58 +0200

    eks-iam

--- myposts/resources/eks-iam.sh
+++ myposts/resources/eks-iam.sh
@@ -4,10 +4,11 @@ command -v kubectl || echo "kubectl must be installed.... exiting" && exit 2
 command -v jq || echo "jq must be installed.... exiting" && exit 2
 command -v aws || echo "aws cli must be installed.... exiting" && exit 2
 
-export CLUSTER="kubectl config view -ojson | jq --arg CTX $(kubectl config current-context) '.contexts | .[] | select(.name == $CTX) | .context.cluster | split("/") | .[length-1]'"
+export CLUSTER="$(kubectl config view -ojson | jq -r --arg CTX $(kubectl config current-context) '.contexts | .[] | select(.name == $CTX) | .context.cluster | split("/") | .[length-1]')"
 export REGION=$(aws configure get region)
-export OIDC=$(aws eks describe-cluster --name ${CLUSTER} --query cluster.identity.oidc | jq '.issuer | split("/") | .[length-1]')
+export OIDC=$(aws eks describe-cluster --name ${CLUSTER} --query cluster.identity.oidc | jq -r '.issuer | split("/") | .[length-1]')
 export ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+echo "cluster $CLUSTER in region $REGION and account number ${ACCOUNT} has OIDC token: ${OIDC}"
 
 curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluentd-quickstart.yaml | sed "s/{{cluster_name}}/${CLUSTER}/;s/{{region_name}}/${REGION}/;s/name: REGION/name: AWS_REGION/;" | kubectl apply -f -
 
